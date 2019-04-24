305f5011ec99ab1f628d0ccc3944b08f42759285
Julian <julianbgs96@gmail.com>
2019-04-24 14:28:34 +0300

    add beauty

--- app/js/script.js
+++ app/js/script.js
@@ -2,15 +2,20 @@ $('#contact-name, #contact-email, #contact-phone, #contact-text').unbind().blur(
 
     var id = $(this).attr('id');
     var val = $(this).val();
-
+    var num_one = $('#num_1');
+    var num_sec = $('#num_2');
+    var num_three = $('#num_3');
+    var num_fourth = $('#num_4');
 
     switch (id) {
         case 'contact-name':
             var rv_name = /^[a-zA-Zа-яА-Я]+ [a-zA-Zа-яА-Я]+$/;
             if (val.length > 2 && val != '' && rv_name.test(val)) {
                 $(this).removeClass('error').addClass('not_error');
+                num_one.css('border','1px solid green');
             } else {
                 $(this).removeClass('not_error').addClass('error');
+                num_one.css('border','1px solid red');
             }
             break;
 
@@ -18,8 +23,10 @@ $('#contact-name, #contact-email, #contact-phone, #contact-text').unbind().blur(
             var rv_email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
             if (val != '' && rv_email.test(val)) {
                 $(this).removeClass('error').addClass('not_error');
+                num_sec.css('border','1px solid green');
             } else {
                 $(this).removeClass('not_error').addClass('error');
+                num_sec.css('border','1px solid red');
             }
             break;
 
@@ -27,16 +34,20 @@ $('#contact-name, #contact-email, #contact-phone, #contact-text').unbind().blur(
             var rv_phone = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
             if (val != '' && rv_phone.test(val)) {
                 $(this).removeClass('error').addClass('not_error');
+                num_three.css('border','1px solid green');
             } else {
                 $(this).removeClass('not_error').addClass('error');
+                num_three.css('border','1px solid red');
             }
             break;
 
         case 'contact-text':
             if (val != '' && val.length < 5000) {
                 $(this).removeClass('error').addClass('not_error');
+                num_fourth.css('border','1px solid green');
             } else {
                 $(this).removeClass('not_error').addClass('error');
+                num_fourth.css('border','1px solid red');
             }
             break;
 
